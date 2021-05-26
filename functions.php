a88e5aaa1cdf2b48bcbbc5a3c32cea8f125c12ab
Charles Harvey <harvey.charles@gmail.com>
2021-05-26 09:28:49 +0100

    no padding top at top

--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
 <?php
+
 /**
  * Churhius Child Theme functions and definitions
  *
  */
 
-if ( !function_exists('churhius_child_enqueue_styles') ) {
+if (!function_exists('churhius_child_enqueue_styles')) {
 
-	add_action( 'wp_enqueue_scripts', 'churhius_child_enqueue_styles' );
+	add_action('wp_enqueue_scripts', 'churhius_child_enqueue_styles');
 
 	function churhius_child_enqueue_styles() {
 		$parent_style = 'churhius-style';
-		wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', array(  'font-linearicons', 'font-churhius-icons', 'font-fontello' ) );
-		wp_enqueue_style( 'churhius-child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+		wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css', array('font-linearicons', 'font-churhius-icons', 'font-fontello'));
+		wp_enqueue_style('churhius-child-style', get_stylesheet_directory_uri() . '/style.css?v=0.0.2', array($parent_style));
 
-		if ( is_rtl() ) {
-			wp_enqueue_style( 'churhius-child-style-rtl', get_stylesheet_directory_uri() . '/rtl.css' );
+		if (is_rtl()) {
+			wp_enqueue_style('churhius-child-style-rtl', get_stylesheet_directory_uri() . '/rtl.css');
 		}
 	}
-
 }
