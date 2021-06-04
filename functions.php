8f75b1cf9d928c712e850496b4540449d0e6e03a
Charles Harvey <harvey.charles@gmail.com>
2021-06-04 12:06:45 +0100

    header padding title

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!function_exists('churhius_child_enqueue_styles')) {
 	function churhius_child_enqueue_styles() {
 		$parent_style = 'churhius-style';
 		wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css', array('font-linearicons', 'font-churhius-icons', 'font-fontello'));
-		wp_enqueue_style('churhius-child-style', get_stylesheet_directory_uri() . '/style.css?v=0.0.3', array($parent_style));
+		wp_enqueue_style('churhius-child-style', get_stylesheet_directory_uri() . '/style.css?v=0.0.4', array($parent_style));
 
 		if (is_rtl()) {
 			wp_enqueue_style('churhius-child-style-rtl', get_stylesheet_directory_uri() . '/rtl.css');
